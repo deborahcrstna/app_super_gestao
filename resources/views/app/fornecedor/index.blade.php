a6fc2da01161dee6bc4d4bf63e6dcf3cd16cbdfa
deborahcrstna <deborahcrstna@gmail.com>
2023-07-18 13:27:13 -0400

    add prefixos blade

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,10 +1,27 @@
 <h3> FORNECEDOR    </h3>
 
-{{--- Aqui fica o comentário descartado pelo interpretador. ---}}
 
 @php
+/*
+if () {
 
-// Comentários usando  PHP puro
+} else if () {
+
+} else {
+
+}
+*/
 
 @endphp
 
+
+
+{{-- SINTAXE DO BLADE PARA CONDIÇÕES ( IF E ELSE ) --}}
+@if (count ($fornecedores) > 0 && count ($fornecedores) < 10 )
+<h3> Existem alguns fornecedores cadastrados. </h3>
+@elseif (count ($fornecedores)> 10)
+<h3> Existem vários fornecedores cadastrados. </h3> 
+@else 
+<h3> Ainda não existem fornecedores cadastrados. </h3> 
+
+@endif
